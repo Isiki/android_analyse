a418bb004da3bb9d1889bb136169243b1d969292
Isiki <Isiki@buaa.edu.cn>
2016-06-26 19:30:08 +0800

    爬虫和下载器更新
    爬虫增加数据量，下载改为循环提取数据库中连接进行下载

--- crawler.py
+++ crawler.py
@@ -4,36 +4,10 @@ from bs4 import  BeautifulSoup
 import urllib2
 import gzip
 import StringIO
-import MySQLdb
+import torndb
 import re
 import multiprocessing
 
-category_url={
-    'books - reference':'http://apk-dl.com/apps/books-reference',
-    'business':'http://apk-dl.com/apps/business',
-    'comics':'http://apk-dl.com/apps/comics',
-    'communication':'http://apk-dl.com/apps/communication',
-    'education': 'http://apk-dl.com/apps/education',
-    'entertainment': 'http://apk-dl.com/apps/entertainment',
-    'finance': 'http://apk-dl.com/apps/finance',
-    'health-fitness': 'http://apk-dl.com/apps/health-fitness',
-    'libraries-demo': 'http://apk-dl.com/apps/libraries-demo',
-    'lifestyle': 'http://apk-dl.com/apps/lifestyle',
-    'media-video': 'http://apk-dl.com/apps/media-video',
-    'medical': 'http://apk-dl.com/apps/medical',
-    'music-audio': 'http://apk-dl.com/apps/music-audio',
-    'news-magazines': 'http://apk-dl.com/apps/news-magazines',
-    'personalization': 'http://apk-dl.com/apps/personalization',
-    'photography': 'http://apk-dl.com/apps/photography',
-    'productivity': 'http://apk-dl.com/apps/productivity',
-    'shopping': 'http://apk-dl.com/apps/shopping',
-    'social': 'http://apk-dl.com/apps/social',
-    'tools': 'http://apk-dl.com/apps/tools',
-    'transportation': 'http://apk-dl.com/apps/transportation',
-    'travel-local': 'http://apk-dl.com/apps/travel-local',
-    'weather': 'http://apk-dl.com/apps/weather',
-    'word': 'http://apk-dl.com/apps/word'
-}
 headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Accept-Encoding': 'gzip, deflate, sdch',
@@ -47,22 +21,18 @@ headers = {
 }
 data = None
 apk_urls=[]
-conn = MySQLdb.connect(host='120.27.92.166', user='root', passwd='112112', port=3306, db='apk')
+
+db = torndb.Connection("120.27.92.166","apk1",user="root",password="112112")
 
 
-def insert(url, name, developer, rate, pacname, category):
+def insert(url, name, developer, rate, package, category):
+    sql = "INSERT INTO apk(url, name, developer, rate, package, category) VALUES (%s,%s,%s,%s,%s,%s)"
     if url[0] != 'h':
         url = 'http:'+url.strip()
     try:
-        cur = conn.cursor()
-        sql = "INSERT INTO apk(url, name, developer, rate, pacname, category) \
-                VALUES ('%s','%s','%s','%f','%s', '%s' )" % \
-              (url.encode('UTF-8'), name, developer, rate, pacname, category.encode('UTF-8'))
-        cur.execute(sql)
-        conn.commit()
-    except:
-        conn.rollback()
-
+        db.insert(sql,url.encode('UTF-8'), name, developer,float(rate), package, category.encode('UTF-8'))
+    except Exception as e:
+        print(e)
 
 def getdoc(url):
     req = urllib2.Request(url, data, headers)
@@ -84,13 +54,14 @@ def extract_apps_from_category(category, url):
                 if re.search(ur"[^\u0000-\u007F]+",url.decode('utf8')) == None:
                     rate = app.find(attrs={'class':'current-rating'})['style'][-4:-2]
                     rate = float(rate) / 20
-                    if rate > 4:
+                    if rate >= 3:
                         get_apk(url, category,rate)
     except Exception as e:
         print e
 
 
 def get_apk(url, category, rate):
+    global conn
     try:
         soup = BeautifulSoup(getdoc('http:'+url), "html5lib")
         apk_info = soup.find("div", class_="info").find_all('div')
@@ -100,21 +71,23 @@ def get_apk(url, category, rate):
         url = soup.find("a", class_="download")['href']
         soup = BeautifulSoup(getdoc('http:'+url.strip()),"html5lib")
         apk_url = soup.find_all('p')[1].find('a')['href']
+        print(apk_url)
         insert(apk_url, name, developer, rate, pacname, category)
     except Exception as e:
-        print e
-
+       print(e)
 
 if __name__ == '__main__':
     pool = multiprocessing.Pool(processes=8)
     progress = multiprocessing.Process()
     progress.start()
-    for key, value in category_url.items():
-        # 分别取5页的数据
-        for page in range(1,11):
-            url = '%s?page=%d' % (value, page)
-            print(url)
-            pool.apply(extract_apps_from_category, (key, url))
+    print "start!!!"
+    categorys = db.query("select * from category")
+    for category in categorys:
+        for currentPage in range(category['page'],50):
+            cate_url = '%s?page=%d' % (category['url'],currentPage)
+            print(cate_url)
+            pool.apply(extract_apps_from_category, (category['name'],cate_url))
+            db.update("update category SET page = %s WHERE name = %s",currentPage+1,category['name'])
     pool.close()
     progress.terminate()
     progress.join()
--- downloader.py
+++ downloader.py
@@ -2,20 +2,17 @@ import torndb
 import os
 import sys
 import requests
+import multiprocessing
 
-SAVE_DIR='.'
-con = torndb.Connection("120.27.92.166", "apk", user="root", password="112112")
+SAVE_DIR='E:/begin_android_english'
+con = torndb.Connection("120.27.92.166", "apk1", user="root", password="112112")
 
 finish_num = 0
 fail_num = 0
-log = open('log.txt', 'w')
-def update(apk,state):
-    try:
-        sql = "UPDATE apk SET state = '%d' WHERE pacname = '%s'" %(state, apk['pacname'])
-        con.update(sql)
-    except Exception as e:
-        log.write(e)
 
+def update(apk,state):
+    sql = "UPDATE apk SET state = %s WHERE package = %s"
+    con.update(sql,state, apk['package'])
 
 def downloader(apk):
     global finish_num
@@ -26,34 +23,42 @@ def downloader(apk):
             apk_url = 'https'+apk_url[4:]
         category = apk['category']
         save_dir=os.path.join(SAVE_DIR, category)
-        filename = apk['name']
+        filename = apk['package']
         save_path=os.path.join(save_dir, filename.strip()+'.apk')
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
-        #if (not os.path.exists(save_path)) or os.stat(save_path).st_size == 0:
-        if os.path.exists(save_path):
+        if not os.path.exists(save_path):
             print ('downloading:' + apk_url + ' as: ' + save_path)
             result = requests.get(apk_url).content
-/           file = open(save_path, 'wb+')
+            file = open(save_path, 'wb+')
             file.write(result)
             file.flush()
             file.close()
             print ('download finish:'+apk_url+' as: '+save_path)
         else:
             print(save_path+'is exits')
-        finish_num += 1
+            finish_num += 1
         update(apk, 1)
     except:
         etype, evalue, tracebackObj = sys.exc_info()[:3]
-        log.write('url:%s errortype:%s errorvalue:%s' % (apk_url, etype, evalue))
-        log.flush()
+        print ('url:%s errortype:%s errorvalue:%s' % (apk_url, etype, evalue))
         fail_num += 1
-        #update(apk, -1)
+        update(apk, -1)
 
-if __name__== '__main__':
-    apks = con.query('select * from apk where state = 1 AND is_malware = 0')
-    for apk in apks:
-        downloader(apk)
-    print ('finish: %d fail %d' %(finish_num,fail_num))
-    print ('finish task')
-    log.close()
+
+if __name__ == '__main__':
+    pool = multiprocessing.Pool(processes=8)
+    progress = multiprocessing.Process()
+    progress.start()
+    while 1:
+        apks = con.query('select * from apk')
+        for apk in apks:
+            downloader(apk)
+        print ('finish: %d fail %d' %(finish_num,fail_num))
+        print ('finish task')
+        if not apks==None:
+            break
+    pool.close()
+    progress.terminate()
+    progress.join()
+    pool.join()
